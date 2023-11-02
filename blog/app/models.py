24ff7b183490f21b5679ccb1b4e34ccc3140f240
Igor Maylov <metal_92@list.ru>
2023-11-03 02:13:29 +0300

    edit migrations

--- blog/app/models.py
+++ blog/app/models.py
@@ -3,10 +3,13 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    published = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         'auth.User',
         on_delete=models.CASCADE,
     )
+    preimage = models.ImageField(upload_to='static/images/')
+    prebody = models.TextField()
     body = models.TextField()
 
     def __str__(self):
