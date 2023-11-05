e3d0fd6be1802f6a4cdfda73efc30de26e9fd620
Igor_Maylov <metal_92@list.ru>
2023-11-06 01:40:36 +0300

    edit style paginator

--- blog/app/views.py
+++ blog/app/views.py
@@ -6,7 +6,7 @@ from .models import Post
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
-    paginate_by = 3
+    paginate_by = 5
 
 
 class BlogDetailView(DetailView):  # новое
